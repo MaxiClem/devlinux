700b6ac133dfcd3bdeeae5cb332cd7ff64edb2d9
HINZ Clement <clhinz@etud.rcisima.isima.fr>
2018-03-08 15:43:01 +0100

    safety

--- affichage.c
+++ affichage.c
@@ -2,29 +2,29 @@
 #include <time.h>
 #include <stdlib.h>
 #include<unistd.h>
-#define TAILLE_X 30
-#define TAILLE_Y 20
-#define NBmonstre 6
+#define TAILLE_X 50
+#define TAILLE_Y 30
+#define NBmonstre 1000
 
 void creerZone (int T[TAILLE_X][TAILLE_Y]){
 	int i;
 	int j;
-	for(i=0; i<30; i++){
+	for(i=0; i<TAILLE_X; i++){
 		T[i][0]=1;
-		T[i][19]=1;
+		T[i][TAILLE_Y-1]=1;
 		
 	}
-	for(j=0; j<20; j++){
+	for(j=0; j<TAILLE_Y; j++){
 		T[0][j]=1;
-		T[29][j]=1;
+		T[TAILLE_X-1][j]=1;
 	}
 
 }
 void affichage (int T[TAILLE_X][TAILLE_Y]){
 	int i;
 	int j;
-	for(j=0; j<20; j++){
-		for(i=0;i<30; i++){
+	for(j=0; j<TAILLE_Y; j++){
+		for(i=0;i<TAILLE_X; i++){
 			switch(T[i][j]){
 				case 1:
 					printf("#");
@@ -44,62 +44,3 @@ void affichage (int T[TAILLE_X][TAILLE_Y]){
 }
 
 
-void monstre(int T[TAILLE_X][TAILLE_Y], int x, int y) {
-	int haut=T[x][y+1];
-	int bas=T[x][y-1];
-	int gauche=T[x-1][y];
-	int droite=T[x+1][y];
-	int move=rand()%4;
-	T[x][y]=0;
-	if (move == 0) {
-		if (haut != 1) {
-			y++;
-		}
-	}
-	if (move == 1) {
-		if (bas != 1) {
-			y--;
-		}
-	}
-	if (move == 2) {
-		if (gauche != 1) {
-			x--;
-		}
-	}
-	if (move == 3) {
-		if (droite != 1) {
-			x++;
-		}
-	}
-	T[x][y]=2;
-}
-void scanmonstre(int T[TAILLE_X][TAILLE_Y]){
-	int i,j;
-	for(i=0;i<TAILLE_Y;i++){
-		for(j=0;TAILLE_X;j++){
-			if(T[j][i]==2){
-				monstre(T,j,i);}
-		}
-	}
-}
-void spawnmonstre(int T[TAILLE_X][TAILLE_Y]){
-	int xrand,yrand,i;
-	for(i=0;i<NBmonstre;i++){
-		xrand=(rand()%(TAILLE_X-3))+1;
-		yrand=(rand()%(TAILLE_Y-3))+1;
-		T[xrand][yrand]=2;}
-}
-
-int main(){
-	srand(time(NULL));
-	int T[TAILLE_X][TAILLE_Y]={{0}};
-	creerZone(T);
-	spawnmonstre(T);
-	while(1==1){
-		system("clear");
-		affichage(T);
-		scanmonstre(T);
-		sleep(1);
-	}
-	return 0;
-}	
--- monstre.c
+++ monstre.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
+#include <time.h>
 #include <stdlib.h>
-#define TAILLE_X 30
-#define TAILLE_Y 20
-
-void monstre(int t[TAILLE_X][TAILLE_Y], int x, int y) {
-	int haut=t[x][y+1];
-	int bas=t[x][y-1];
-	int gauche=t[x-1][y];
-	int droite=t[x+1][y];
-	int move=rand()%4;
-	t[x][y]=0;
+#include<unistd.h>
+#define TAILLE_X 50
+#define TAILLE_Y 30
+#define NBmonstre 1000
+void monstre(int T[TAILLE_X][TAILLE_Y], int x, int y) {
+	int move=0;
+	int haut=T[x][y+1];
+	int bas=T[x][y-1];
+	int gauche=T[x-1][y];
+	int droite=T[x+1][y];
+	move=rand()%4;
+	T[x][y]=0;
 	if (move == 0) {
 		if (haut != 1) {
 			y++;
@@ -30,7 +33,25 @@ void monstre(int t[TAILLE_X][TAILLE_Y], int x, int y) {
 			x++;
 		}
 	}
-	t[x][y]=2;
+	T[x][y]=2;
+}
+void scanmonstre(int T[TAILLE_X][TAILLE_Y]){
+	int i,j;
+	for(i=0;i<TAILLE_X;i++){
+		for(j=0;j<TAILLE_Y;j++){
+			if(T[i][j]==2){
+				monstre(T,i,j);
+			}
+		}
+	}
+}
+
+void spawnmonstre(int T[TAILLE_X][TAILLE_Y]){
+	int xrand,yrand,i;
+	for(i=0;i<NBmonstre;i++){
+		xrand=(rand()%(TAILLE_X-3))+1;
+		yrand=(rand()%(TAILLE_Y-3))+1;
+		T[xrand][yrand]=2;}
 }
 
 
